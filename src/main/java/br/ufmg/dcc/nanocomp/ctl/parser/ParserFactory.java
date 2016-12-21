52dcf2d59c48fe1caa416316abf789b45f05caa7
henriquenicolas <henriquenicolas@outlook.com.br>
2016-12-21 21:44:59 -0200

    CTL Parser

---
+++ src/main/java/br/ufmg/dcc/nanocomp/ctl/parser/ParserFactory.java
@@ -0,0 +1,43 @@
+package br.ufmg.dcc.nanocomp.ctl.parser;
+
+
+import java.util.Scanner;
+
+import br.ufmg.dcc.nanocomp.peg.PEG;
+import br.ufmg.dcc.nanocomp.peg.Parser;
+
+public class ParserFactory {
+
+	private Parser<CtlFile> parser;
+	private String source;
+	private static ParserFactory instance = new ParserFactory();
+
+	/**
+	 * This class will be a Singleton, when constructing it will read the Grammars and compile the scripts
+	 */
+	@SuppressWarnings("unchecked")
+	private ParserFactory() {
+		try (Scanner grammar = new Scanner(ParserFactory.class.getResourceAsStream("/CtlGrammar.peg"))){
+			parser = PEG.getInstance().generate(grammar.useDelimiter("\\Z").next(),Parser.class);
+		} catch (Exception e){
+			throw new RuntimeException(e);
+		}
+	}
+
+	/**
+	 * Method to recover the Singleton
+	 * @return
+	 */
+	public static ParserFactory getInstance() {
+		return instance;
+	}
+
+	public Parser<CtlFile> getParser() {
+		return parser;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+}
